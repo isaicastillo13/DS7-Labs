27a1f4a51995be4ef3d679e3de87152a64a9d13d
Isaias Castillo <isaias.castillo@utp.ac.pa>
2023-09-06 12:22:29 -0500

    laboratorio 1 finalizado

---
+++ Laboratorio 1/lab1.2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.2</title>
+</head>
+<body>
+    <?php
+    $num1 = 13;
+    $num2 = 16;
+    $suma = $num1 + $num2;
+    echo "<h2> la Suma de los 2 numero es de: $suma </h2>";
+    ?>
+</body>
+</html>
