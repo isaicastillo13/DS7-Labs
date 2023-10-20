f66391d52fd68efaec41b1602b33c90cc51b66e9
Isaias Castillo <isaias.castillo@utp.ac.pa>
2023-10-20 14:17:06 -0500

    Agrego el laboratorio 11 terminado

---
+++ Laboratorio 11/class/noticias.php
@@ -0,0 +1,54 @@
+<?php
+    require_once('modelo.php');
+
+    class noticias extends modeloCredencialesDB{
+
+        protected $titulo;
+        protected $texto;
+        protected $categoria;
+        protected $fecha;
+        protected $imagen;
+        protected $limite;
+        protected $offset;
+
+        
+
+
+
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function consultar_noticias($limite,$offset){
+
+            $sql = "call sp_listar_noticias_limit('".$limite."','".$offset."')";
+            $consulta = $this->_db->query($sql);
+            //Obtengo todas las filas que me trajo la consulta
+            $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+            //print_r($resultado);
+            if(!$resultado){
+                echo "Fallo la consulta";
+            }else{
+                return $resultado;
+                $resultado->close();
+                $this->_db->close();
+            }
+        }
+
+        public function consultar_noticias_filtro($campo,$valor){
+            $sql="call sp_listar_noticias_filtro('".$campo."','".$valor."')";
+
+            $consulta = $this->_db->query($sql);
+            $resultado = $consulta->fetch_all(MYSQLI_ASSOC);
+
+            //var_dump($consulta);
+
+            if($resultado){
+                return $resultado;
+                $resultado->close();
+                $this->_db->close();
+            }
+
+        }
+    }
+?>
